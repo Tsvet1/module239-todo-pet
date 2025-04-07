94f490ab3efca98cadbbd4947e4f184a9ee47a79
TsvetIPetrov <120581383+Tsvet1@users.noreply.github.com>
2025-04-07 14:43:13 +0200

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://ca-chy-300233-239-tsvetipetrov.bluetree-d1cd7844.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
